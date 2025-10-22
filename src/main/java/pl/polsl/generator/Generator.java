38ac78a3441cea1af678203ce56824e9fcbefcaf
Użytkownik <Uytkownik@KAMIL-KOMPUTER>
2025-10-22 22:48:43 +0200

    Project was initialized.

---
+++ src/main/java/pl/polsl/generator/Generator.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ */
+
+package pl.polsl.generator;
+
+/**
+ *
+ * @author Kay Jay O'Nail
+ */
+public class Generator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
